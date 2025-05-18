ad0f36da65ddc439b9bbb2607b1756135029beea
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-05-18 17:25:42 +0500

    feat(search): enhance FamousCityTile UI and add project screenshots to README.

--- README.md
+++ README.md
@@ -3,5 +3,11 @@
 ## Description
 This project is a straightforward weather application developed using Flutter. It utilizes the Weather API to retrieve and display current weather data. Created primarily for learning purposes to practice Flutter UI development and API consumption.\
 
+## Screenshots
+<p float="left">
+<img src="docs/screenshot/home_page.png" width="200"/>
+<img src="docs/screenshot/search_page.png" width="200"/>
+</p>
+
 ## Resources
 Youtube: [Flutter Weather App Tutorial | Dio x Riverpod Tutorial for beginners](https://youtu.be/msoKuk-5QFg)
--- lib/features/search/widgets/famous_city_tile.dart
+++ lib/features/search/widgets/famous_city_tile.dart
@@ -19,19 +19,43 @@ class FamousCityTile extends ConsumerWidget {
           elevation: index == 0 ? 8 : 0,
           borderRadius: BorderRadius.circular(25),
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 24),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   children: [
-                    Column(
-                      children: [
-                        Text(weather.main.temp.round().toString()),
-                        const SizedBox(height: 10),
-                        Text(weather.weather[0].description),
-                      ],
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${weather.main.temp.round()}°',
+                            style: AppTextStyles.h2,
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            weather.weather[0].description,
+                            style: AppTextStyles.subtitleText,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ],
+                      ),
                     ),
+                    getWeatherIcon(
+                      weatherCode: weather.weather[0].id,
+                    ).image(width: 50),
                   ],
+                ),
+                Text(
+                  weather.name,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white.withValues(alpha: 0.8),
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
               ],
             ),
